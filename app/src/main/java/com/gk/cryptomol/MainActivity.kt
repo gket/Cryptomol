e00c7d5cd38409e0cb99b11da20f736ec9c90527
“gkethuda” <gorkem@mobven.com>
2020-07-21 19:32:39 +0300

    Initial commit

---
+++ app/src/main/java/com/gk/cryptomol/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.gk.cryptomol
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.navigationContainer)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/gk/cryptomol/ui/favorites/FavoriteFragment.kt
@@ -0,0 +1,31 @@
+package com.gk.cryptomol.ui.favorites
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.gk.cryptomol.R
+
+class FavoriteFragment : Fragment() {
+
+    private lateinit var favoriteViewModel: FavoriteViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        favoriteViewModel =
+                ViewModelProviders.of(this).get(FavoriteViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        favoriteViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
